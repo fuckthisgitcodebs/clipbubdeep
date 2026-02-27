3852e99b724dc7f6493fa410c698bf030ed5ec40
fuckthisgitcodebs <75880328+fuckthisgitcodebs@users.noreply.github.com>
2026-02-26 19:58:31 -0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
-id("com.android.application")
-id("org.jetbrains.kotlin.android")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.haunted421.textcommandoverlay"
-
     compileSdk = 34
 
     defaultConfig {
@@ -17,16 +16,14 @@ android {
     }
 }
 
-}
-
 dependencies {
-implementation("androidx.core:core-ktx:1.13.1")
-implementation("androidx.appcompat:appcompat:1.7.0")
-implementation("com.google.android.material:material:1.12.0")
-implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-implementation("androidx.preference:preference-ktx:1.2.1")
-implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-testImplementation("junit:junit:4.13.2")
-androidTestImplementation("androidx.test.ext:junit:1.2.1")
-androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
